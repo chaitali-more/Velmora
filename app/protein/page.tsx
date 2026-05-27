79a68e4f8eea8400917500e22d972aa9926ad5c4
Chaitali More <chaitali@dotscoms.com>
2026-05-27 18:17:07 +0530

    Error SOlved

--- app/protein/page.tsx
+++ app/protein/page.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { buildCalculatorMetadata } from "@/lib/seo";
-import ProteinClientPage from "./ProteinClientPage";
+import ProteinClientPage from "../protein-calculator/ProteinClientPage";
 
 export const metadata: Metadata = buildCalculatorMetadata({
   title: "Protein Calculator - Daily Protein Intake by Weight",
